808ff5ee6f3e9f49a23ab3a1aadfae4034549295
Phil Travis <physicistphil@gmail.com>
2019-04-22 17:21:42 -0700

    Added docstrings to __init__ in the FFNN class and the sigmoid function. Implemented changes to allow 0-depth (perceptron) neural networks.

--- simple_nn.py
+++ simple_nn.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def sigmoid(x, derivative=False):
+    """Return the sigmoid (or derivative of sigmoid, if chosen) of x."""
     if derivative:
         return sigmoid(x) * (1 - sigmoid(x))
     else:
@@ -10,17 +11,38 @@ def sigmoid(x, derivative=False):
 
 # just doing sigmoid activation functions because it's easier
 class FeedForwardNeuralNetwork:
+    """Contains the necessary information and algorithms to train a simple logistic feedforward neural network"""
+
     def __init__(self, hidden_width, depth, input_size, output_size):
+        """Initialize the class and create the network.
+
+        Args:
+            hidden_width (int): the width of the hidden layers of the network.
+            depth (int): the number of hidden layers in the network.
+            input_size (int): the number of features in the input dataset.
+            output_size (int): the number of outputs.
+
+        Creates the network as a list of numpy arrays.
+        Weights are assigned from a Gaussian distribution with mean 0 and standard deviation 0.
+        """
         self.width = hidden_width
         self.depth = depth
         self.network = []
 
         # activations are in columns
         # minimum one hidden layer
+        # perceptron
+        if depth == 0:
+            self.network.append((np.random.normal(0, 1, (input_size + 1, output_size))))
+            return
+
+        # deep NN
         self.network.append(np.random.normal(0, 1, (input_size + 1, hidden_width)))
-        for i in range(depth):
+        for i in range(depth - 1):
             self.network.append(np.random.normal(0, 1, (self.network[-1].shape[1] + 1, hidden_width)))
         self.network.append(np.random.normal(0, 1, (self.network[-1].shape[1] + 1, output_size)))
+
+        return
 
     def feed_forward(self, dataset):
 
@@ -49,9 +71,7 @@ class FeedForwardNeuralNetwork:
         h = sigmoid(self.feed_forward(dataset_input) - dataset_output)
 
         # logistic cost function
-        J = np.sum(-dataset_output * np.log(h) - (1 - dataset_output) * np.log(1 - h)) / dataset_input.shape[0]
-        J_reg = weight_decay / 2 * sum([np.sum(np.square(arr)) for arr in self.network]) / dataset_input.shape(0)
+        j = np.sum(-dataset_output * np.log(h) - (1 - dataset_output) * np.log(1 - h)) / dataset_input.shape[0]
+        j_reg = weight_decay / 2 * sum([np.sum(np.square(arr)) for arr in self.network]) / dataset_input.shape(0)
 
-        return J + J_reg
-
-
+        return j + j_reg
